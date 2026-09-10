8c195369085363f26a6918333af1dd084f0f6602
Aarav01go <aaravg901@gmail.com>
2026-09-10 11:40:52 +0530

    Initial commit

---
+++ dommain manupulation/index.js
@@ -0,0 +1,10 @@
+const root = document.getElementById("root");
+const btn = document.getElementById("btn");
+console.log(btn);
+console.log(root);
+
+function getData(){
+    alert("hii");
+}
+
+button.addEventListener('click',getData);
---
+++ new/index.js
@@ -0,0 +1,9 @@
+const container=document.getElementById('root');
+
+const root=ReactDom.createRoot(container);
+const h2 =React.createElement('h2',{style:{color:'red',backgroundColor:'black'}},'Welcome');
+const h1 =React.createElement('h1',{},'ABES EC');
+('img',{src:'{,style:'})
+const div=React.createElement('div',{style:{border:'2px solid red'}},h1,h2);
+
+root.render(h2);
