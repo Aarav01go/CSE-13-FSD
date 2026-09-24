e1dafe9043af4874f034eafe8d8dd72377d49bd0
Aarav01go <aaravg901@gmail.com>
2026-09-24 13:03:05 +0530

    CSE-13 FSD: All project files (DOM manipulation, React app, useFetch)

---
+++ fsd-13/react/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import ICardGallery from './component/ICardGallery'
+import ImdbCard from './component/ImdbCard'
+
+function App() {
+  
+
+  return (
+    <div>
+      {/* < ICardGallery /> */}
+      <ImdbCard/>
+    </div>
+  )
+}
+
+export default App
